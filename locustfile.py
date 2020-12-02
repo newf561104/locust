2fa69aeb6ef58ae26d3f87167e3e253c64b275cf
newf561104 <39904689+newf561104@users.noreply.github.com>
2020-12-03 06:00:08 +0900

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -4,8 +4,5 @@ from locust import HttpUser, task
 class QuickstartUser(HttpUser):
     @task
     def simple_call(self):
-        res = self.client.get("?card_code=rush_test_101&status=&metadata=&button_log=", headers={'referer': 'rush_test_referer'})
+        res = self.client.get("", headers={'referer': 'rush_test_referer'})
         time.sleep(1)
-
-    # def on_start(self):
-    #     self.client.post("/login", json={"username":"foo", "password":"bar"})
