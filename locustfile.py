4f60529ba649fc58885851efdee065d9bc64bd72
newf561104 <39904689+newf561104@users.noreply.github.com>
2020-12-03 13:30:47 +0900

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,14 @@
 from locust import HttpUser, TaskSet, task, between, constant
+import json
 
 class UserBehavior(TaskSet):
     @task(1)
     def get(self):
-        self.client.get("", headers={'referer': 'rush_test_referer'})
-
+        with self.client.get("", headers={'referer': 'rush_test_referer'}, catch_response = True) as response:
+            if response.status_code != 200:
+                response.failure("Failed.")
+            if json.loads(response.text)['result_code'] != '00101':
+                response.failure("scenario does not load.")
 class WebsiteUser(HttpUser):
     tasks = {UserBehavior:1}
     wait_time = constant(1)
